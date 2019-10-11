5c4ea9cd673a04195cff7805f57e03792fc5da51
rskrz <ryanskrzypek@gmail.com>
2019-10-11 14:14:27 -0500

    added jetpack cat logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
-  div(id="Home") Hello World!
+  div(id="Home")
+    div.flex.center.items-center.justify-center.banner.h5
+      h1 JETPACK
+      img(src="../assets/kot2.png").pa3.w5
+      h1.ml4 CAT
+    div Functionality
 </template>
 
 <script lang="ts">

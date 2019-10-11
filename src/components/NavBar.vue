5573aca63d12f610c8169f91583bf556448a730b
rskrz <ryanskrzypek@gmail.com>
2019-10-10 23:01:49 -0500

    create website

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .h3.w-100.flex.flex-row.justify-end-ns.justify-center.items-center.router
+    router-link(to="/").mr4-ns.mh2 Home
+    router-link(to="../Commands").mr4-ns.mh2 Commands
+    router-link(to="../Invite").mr4-ns.mh2 Invite
+    router-link(to="../GitHub").mr4-ns.mh2 GitHub
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  name: "NavBar"
+})
+export default class NavBar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+#navbar {
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div(id="Home") Hello World!
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default class Home extends Vue {}
+</script>
+
+<style scoped lang="scss">
+#Home {
+}
+</style>
